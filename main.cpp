7e5941e892b440b640dcd81f96d42251e1bb63df
amcspps <pashafizmat1@gmail.com>
2023-12-25 17:42:40 +0300

    cmd input args

--- main.cpp
+++ main.cpp
@@ -3,9 +3,22 @@
 
 auto main(int argc, char** argv) -> int {
     IntQueue queue;
-    WriterTests::writer_test(queue, 2, 10);
+    if (argc != 3) {
+        std::cerr << "usage: " << argv[0] << " <thread count> <values per operator>" << std::endl;
+        return 1;
+    }
+
+    int thread_count = std::atoi(argv[1]);
+    int values_per_operator = std::atoi(argv[2]);
+    
+    if (thread_count == 0|| values_per_operator == 0) {
+        std::cerr << "invalid arguments (must be not null)" << std::endl;
+        return 1; 
+    }
+    
+    WriterTests::writer_test(queue, thread_count, values_per_operator); 
     Utils::clear(queue);
-    ReaderTests::reader_test(queue, 2, 10);
+    ReaderTests::reader_test(queue, thread_count, values_per_operator); 
     Utils::clear(queue);
     CommonTests::common_test(queue);
     return 0;
